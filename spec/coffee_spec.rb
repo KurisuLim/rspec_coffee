1c0e20bc3876807bcb757a7b65f15437c1bbb06e
KurisuLim <lim@Christophers-MacBook-Pro.local>
2018-10-16 15:32:36 -0700

    adds marking incomplete work

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -24,11 +24,20 @@ class Coffee
       expect(coffee.price).to eq(1.00)
     end
   
-    fcontext 'with milk' do
+    context 'with milk' do
       before { coffee.add :milk }
   
       it 'costs $1.25' do
         expect(coffee.price).to eq(1.25)
       end
-    end
+
+      it 'is light in color' do
+        pending 'Color not implemented yet'
+        expect(coffee.color).to be(:light)
+      end
+      it 'is cooler than 200 degrees Fahrenheit' do
+        pending 'Temperature not implemented yet'
+        expect(coffee.temperature).to be < 200.0
+      end
   end
+end
